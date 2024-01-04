60fe739363d008f6e82911389993c676fee6416d
Mayrab01 <94092491+Mayrab01@users.noreply.github.com>
2024-01-04 13:13:31 -0600

    Update: resume

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -216,7 +216,7 @@ const Resume = ({ coolersData }) => {
               <Tabs.Panel value="first" pt="xs">
                 <div
                   style={{
-                    width: "500px",
+                    width: "95%",
                     height: "450px",
                     flexShrink: 0,
                     borderRadius: "8px",
@@ -351,7 +351,7 @@ const Resume = ({ coolersData }) => {
               <Tabs.Panel value="second" pt="xs">
                 <div
                   style={{
-                    width: "500px",
+                    // width: "500px",
                     display: "flex",
                     padding: "8px 0px",
                     justifyContent: "space-between",
