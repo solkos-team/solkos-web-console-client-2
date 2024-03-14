add3c96f5166644420108a307b9071c829443fdb
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2024-03-14 09:30:35 -0600

    Update MapInsights
    Update MapInsights / Add clusters in to map

--- src/components/mapInsights/MapInsights.tsx
+++ src/components/mapInsights/MapInsights.tsx
@@ -23,12 +23,13 @@ const MapInsightsComponent = ({ items, data }) => {
     page_size: items > 1000 || items === 0 ? 1000 : items,
     page_number: 1,
   };
+  const body = { customer: dto, path: pathVerify() };
   const fetchData2 = async () => {
     try {
-      const data = await fetchUniversal("coolers", body2, setIsLoading);
+      const data = await fetchUniversal("insights", body, setIsLoading);
       setMarkers(
         data
-          ? data
+          ? data.geo_data
               .filter(
                 (cooler) =>
                   parseFloat(cooler.latitude) != 0 &&
@@ -37,6 +38,8 @@ const MapInsightsComponent = ({ items, data }) => {
               .map((cooler) => ({
                 latitude: parseFloat(cooler.latitude),
                 longitude: parseFloat(cooler.longitude),
+                num_coolers : cooler.num_coolers,
+                area : cooler.area              
               }))
           : []
       );
@@ -49,27 +52,40 @@ const MapInsightsComponent = ({ items, data }) => {
   useEffect(() => {
     fetchData2();
   }, [dt, dto]);
-
+  
+  const [scale , setScale ] = useState(24)
   const handleApiLoaded = (map, maps) => {
     setMap(map);
     const bounds = new maps.LatLngBounds();
+    var mIcon = {
+      path: maps.SymbolPath.CIRCLE,
+      fillOpacity: 0.8,
+      fillColor: '#BCDAFF',
+      strokeOpacity: 0.0,
+      strokeWeight: 1,      
+      scale: Number(scale),       
+    };
     markers === undefined
       ? []
-      : markers.forEach(({ latitude, longitude }, index) => {
+      : markers.forEach(({ latitude, longitude, num_coolers,area }, index) => {
           const marker = new maps.Marker({
             position: { lat: latitude, lng: longitude },
             map,
-            title: `Marcador ${index + 1}`,
-            icon: {
-              path: maps.SymbolPath.CIRCLE,
-              fillColor: "#ec5078", // Color de relleno del círculo
-              fillOpacity: 1.0,
-              scale: 2, // Tamaño del círculo
-              strokeColor: "#000000", // Color del borde del círculo
-              strokeWeight: 0.1, // Grosor del borde del círculo
-            },
+            title: `${area}  ${num_coolers.toLocaleString('es-MX')} Coolers`,
+            icon: mIcon,
+            label : {
+              color: '#000', fontSize: '12px', fontWeight: '600',
+              text: `${num_coolers.toLocaleString('es-MX')}`
+            },            
           });
 
+          marker.addListener('mouseover', e => {
+            marker.setIcon({ mIcon, scaledSize: 44 })            
+          })
+
+          marker.addListener('mouseout', e => {
+            setScale(24)
+          })
           bounds.extend(marker.getPosition());
         });
 
@@ -82,15 +98,15 @@ const MapInsightsComponent = ({ items, data }) => {
         width: "100%",
         height: "100%",
         flexShrink: 0,
-        borderRadius: "8px",
-        border: "1px solid #CACACA",
+        borderRadius: "0.5rem",
+        // border: "1px solid #CACACA",
         overflow: "hidden",
       }}
     >
       {isLoading === true ? (
         <SkeletonMapInsights />
       ) : (
-        <GoogleMapReact
+        <GoogleMapReact      
           bootstrapURLKeys={{
             key: "AIzaSyBYTHbWcKL5Apx4_l9_eM-LcRZlMXWjl2w",
           }}
@@ -99,7 +115,7 @@ const MapInsightsComponent = ({ items, data }) => {
               ? { lat: markers[0].latitude, lng: markers[0].longitude }
               : { lat: 0, lng: 0 }
           }
-          defaultZoom={20}
+          defaultZoom={30}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
           options={{
@@ -109,19 +125,19 @@ const MapInsightsComponent = ({ items, data }) => {
         />
       )}
 
-      <div
+      {/* <div
         style={{
           position: "absolute",
           top: "25px",
           right: "10px",
         }}
       >
-        {/* <IconMap
+        <IconMap
           size={30}
           style={{ color: "#666666", cursor: "pointer" }}
           onClick={openGoogleMaps}
-        /> */}
-      </div>
+        />
+      </div> */}
     </div>
   );
 };
