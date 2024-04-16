a7ac0123f43227864e04fc70bece261e01ea7dc8
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2024-04-16 13:42:22 -0600

    Add CoolView component
    Add CoolView component

---
+++ src/pages/shell/coolView/coolView.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
+export const CoolView = () => {
+    const [deviceId, setDeviceId] = useState('');
+    const [dateFrom, setDateFrom] = useState('');
+    const [dateTo, setDateTo] = useState('');
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const deviceIdParam = urlParams.get('device_id') || '';
+        const dateFromParam = urlParams.get('date_from') || '';
+        const dateToParam = urlParams.get('date_to') || ''; 
+        setDeviceId(deviceIdParam);
+        setDateFrom(dateFromParam);
+        setDateTo(dateToParam);
+    }, []);
+
+    return (
+        <iframe 
+        src={`http://localhost:5174/`} 
+        width='100%' 
+        height='100%' 
+        frameBorder='0'
+        style={{marginTop:'-2rem',marginLeft:'-4rem'}}
+        />
+        // <iframe 
+        // src={`https://solkos-data-viewer-b7agk5thba-uc.a.run.app/?device_id=${deviceId}&date_from=${dateFrom}&date_to=${dateTo}`} 
+        // width='100%' 
+        // height='100%' 
+        // frameBorder='0'
+        // />
+    )
+}
