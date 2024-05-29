dbdeca500dc305a73bfdd553b2e9c891cfb6d0e3
Mayrab01 <94092491+Mayrab01@users.noreply.github.com>
2024-05-29 17:52:44 -0600

    Update: update universal search

--- src/pages/shell/clt/Detail.tsx
+++ src/pages/shell/clt/Detail.tsx
@@ -1,8 +1,103 @@
+// import React, { useState, useEffect } from "react";
+// import { useParams, useNavigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import PageFilter from "../../../components/pageFilter";
+// import { Tooltip, Skeleton, Center } from "@mantine/core";
+// import { Link } from "react-router-dom";
+// import { fetchUniversalDetails } from "../../../utils/apiUtils";
+// import moment from "moment";
+// import "moment/locale/es";
+// import { CoolerData } from "../../../interfaces/CoolerInterface";
+// import MapComponent from "../../../components/map";
+// import MapComponent1 from "../../../components/map_1";
+// import MapComponent2 from "../../../components/map_2";
+// import { IconArrowRight } from "@tabler/icons-react";
+// import DrawerInversion from "../../../components/drawerInversion/DrawerInversion";
+// import DrawerEnergy from "../../../components/drawerEnergy/DrawerEnergy";
+// import { useDisclosure } from "@mantine/hooks";
+
+// moment.locale("es", {
+//   months: [
+//     "Enero",
+//     "Febrero",
+//     "Marzo",
+//     "Abril",
+//     "Mayo",
+//     "Junio",
+//     "Julio",
+//     "Agosto",
+//     "Septiembre",
+//     "Octubre",
+//     "Noviembre",
+//     "Diciembre",
+//   ],
+// });
+
+// export default function CoolerDetail() {
+//   // ***************
+
+//   // ***************
+//   const dto = useSelector((state: any) => state.organization);
+//   const b = "../../sampleData/devices.png";
+//   const { serial_number } = useParams();
+//   const [searchValue, setSearchValue] = useState("");
+//   const [coolersData, setCoolersData] = useState<CoolerData | null>(null);
+//   const [editSerie, setEditSerie] = useState(false);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [serie, SetSerie] = useState(serial_number);
+//   const [inversionOpened, { open: openInversion, close: closeInversion }] =
+//     useDisclosure(false);
+//   const [energyOpened, { open: openEnergy, close: closeEnergy }] =
+//     useDisclosure(false);
+
+//   const fetchData = async (serie?) => {
+//     try {
+//       const data = await fetchUniversalDetails(
+//         "coolers",
+//         serie,
+//         "GET",
+//         setIsLoading
+//       );
+//       setCoolersData(data);
+//       console.log(data);
+//       setIsLoading(false);
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }
+//   };
+//   const [fechaActual, setFechaActual] = useState("");
+//   const [fechaAnterior, setFechaAnterior] = useState("");
+//   function getFechas() {
+//     var fechaActual = new Date();
+//     var fechaAnterior = new Date(
+//       fechaActual.getFullYear(),
+//       fechaActual.getMonth(),
+//       fechaActual.getDate() - 30
+//     );
+//     var fechaActualFormateada = fechaActual.toISOString().split("T")[0];
+//     var fechaAnteriorFormateada = fechaAnterior.toISOString().split("T")[0];
+//     setFechaActual(fechaActualFormateada);
+//     setFechaAnterior(fechaAnteriorFormateada);
+//   }
+//   useEffect(() => {
+//     fetchData(serie);
+//     getFechas();
+//   }, []);
+
+//   useEffect(() => {}, [serial_number, coolersData]);
+//   const searchSerial = (value) => {
+//     value == "" || value == null || value == undefined
+//       ? alert("Ingresa datos correctos! ")
+//       : fetchData(value);
+//     setEditSerie(false);
+//   };
+
 import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import PageFilter from "../../../components/pageFilter";
 import { Tooltip, Skeleton, Center } from "@mantine/core";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { fetchUniversalDetails } from "../../../utils/apiUtils";
 import moment from "moment";
 import "moment/locale/es";
@@ -61,8 +156,10 @@ export default function CoolerDetail() {
       console.error("Error:", error);
     }
   };
+
   const [fechaActual, setFechaActual] = useState("");
   const [fechaAnterior, setFechaAnterior] = useState("");
+
   function getFechas() {
     var fechaActual = new Date();
     var fechaAnterior = new Date(
@@ -75,12 +172,17 @@ export default function CoolerDetail() {
     setFechaActual(fechaActualFormateada);
     setFechaAnterior(fechaAnteriorFormateada);
   }
+
   useEffect(() => {
     fetchData(serie);
     getFechas();
   }, []);
 
-  useEffect(() => {}, [serial_number, coolersData]);
+  useEffect(() => {
+    SetSerie(serial_number);
+    fetchData(serial_number);
+  }, [serial_number]);
+
   const searchSerial = (value) => {
     value == "" || value == null || value == undefined
       ? alert("Ingresa datos correctos! ")
