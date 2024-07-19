08b7bfdd68279609ca3c5adbdd833ba45687b481
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2024-07-19 13:26:55 -0600

    Update Coolview Drawer
    Update Coolview Drawer , add new date to show data

--- src/pages/shell/coolView/coolviewDrawer.tsx
+++ src/pages/shell/coolView/coolviewDrawer.tsx
@@ -4,18 +4,20 @@ import { formatDrawerCoolview, getMonth, obtenerFechas } from "../../../Function
 import { LogoCoolview } from "../../../sampleData/Coolview/coolviewIcons"; 
 export const coolviewDrawer = ({ opened, onClose, CoolerId,mesLastStat,dateStat }) => {  
   const [URL,setURL] = useState<string>('')  
+  const mesToday = new Date();
   if(dateStat != undefined && URL == ''){  
   setURL(`https://solkos-coolview-2.firebaseapp.com?device_id=${CoolerId}&start_date=${            
             obtenerFechas(dateStat.getUTCMonth() + 1,dateStat.getFullYear())[1]
           }&end_date=${obtenerFechas(dateStat.getUTCMonth() + 1,dateStat.getFullYear())[0]}&clt=false`)
   }
   
-  const mesToday = new Date();
+  
 
   useEffect(() => {
     formatDrawerCoolview()
   }, [opened]);
   formatDrawerCoolview()    
+  console.log(dateStat)
   return (
     dateStat !== undefined 
    ?
@@ -38,7 +40,27 @@ export const coolviewDrawer = ({ opened, onClose, CoolerId,mesLastStat,dateStat 
         />
       </section>
     </Drawer> 
-   : <h1>Esperando....</h1> 
+   : <Drawer
+   opened={opened}
+   onClose={onClose}
+   title={<img src={LogoCoolview} alt={'LogoCoolview'} className="CoolviewLogo"></img>}
+   position="bottom"
+   size="76%"
+   overlayOpacity={0.1}
+   overlayBlur={0}
+   className="drawerCoolview"
+ >
+   <section className="coolview_printipal">  
+     <iframe
+       src={`https://solkos-coolview-2.firebaseapp.com?device_id=${CoolerId}&start_date=${
+            getMonth(mesToday.getMonth(),mesToday.getFullYear()).firstDay
+          }&end_date=${getMonth(mesToday.getMonth() + 1,mesToday.getFullYear()).lastDay}&clt=false`}
+       width="100%"
+       height="100%"
+       frameBorder="0"
+     />
+   </section>
+ </Drawer> 
   )
 };
 
