8b3387edcf2c4f6473cef7a9101bf0e3de3e770c
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2024-09-20 10:36:05 -0600

    Update Drawer UI Coolview
    Update Drawer UI Coolview Animation

--- src/sampleData/Coolview/CoolviewIcons.ts
+++ src/sampleData/Coolview/CoolviewIcons.ts
@@ -2,5 +2,5 @@
 import LogoCoolview from '../Coolview/CoolviewPrincipalLogoDrawer.svg'
 //@ts-ignore
 import SolkosSymbol from '../Coolview/solkosSymbol.svg'
-const CoolviewAnimationOpen = 'https://lottie.host/4358e738-3204-44b3-b218-86cdb58fff71/1cF1qrHTcY.json'
+const CoolviewAnimationOpen = 'https://lottie.host/426c3d99-77a7-4fe2-b93b-c0c33e99dd12/iTzwM6KTqF.json'
 export { LogoCoolview,SolkosSymbol,CoolviewAnimationOpen}
