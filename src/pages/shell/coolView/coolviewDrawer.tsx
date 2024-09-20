bf8d81e946e931ce8ff77e45a8b453c5875b0878
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2024-09-20 10:33:37 -0600

    Update Drawer UI coolview
    Update Drawer UI Coolview

--- src/pages/shell/coolView/coolviewDrawer.tsx
+++ src/pages/shell/coolView/coolviewDrawer.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Alert, Drawer } from "@mantine/core";
-import {formatDrawerCoolview,getDates,updateTelemetriaStatus,} from "../../../Functions/Coolview";
-import {CoolviewAnimationOpen, LogoCoolview,} from "../../../sampleData/Coolview/CoolviewIcons";
+import { getDates, updateTelemetriaStatus, } from "../../../Functions/Coolview";
+import { CoolviewAnimationOpen, LogoCoolview, } from "../../../sampleData/Coolview/CoolviewIcons";
 import { IconInfoCircle } from "@tabler/icons-react";
-import {Player} from '@lottiefiles/react-lottie-player'
+import { Player } from '@lottiefiles/react-lottie-player'
 
 export const coolviewDrawer = ({
   opened,
@@ -14,28 +14,26 @@ export const coolviewDrawer = ({
 }) => {
   const [telemetriaStatus, setTelemetriaStatus] = useState(true);
   const [URL, setURL] = useState<string>("");
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const mesToday = new Date();
   if (dateStat != undefined && URL == "") {
     setURL(
-      `https://solkos-coolview-2.firebaseapp.com?device_id=${CoolerId}&start_date=${
-        getDates(
-          dateStat.getDate() + 1,
-          dateStat.getMonth() + 1,
-          dateStat.getFullYear()
-        )[1]
-      }&end_date=${
-        getDates(
-          dateStat.getDate() + 1,
-          dateStat.getMonth() + 1,
-          dateStat.getFullYear()
-        )[0]
+      `https://solkos-coolview-2.firebaseapp.com?device_id=${CoolerId}&start_date=${getDates(
+        dateStat.getDate() + 1,
+        dateStat.getMonth() + 1,
+        dateStat.getFullYear()
+      )[1]
+      }&end_date=${getDates(
+        dateStat.getDate() + 1,
+        dateStat.getMonth() + 1,
+        dateStat.getFullYear()
+      )[0]
       }&clt=false`
     );
   }
 
   updateTelemetriaStatus(dateStat, telemetriaStatus, setTelemetriaStatus);
-  
+
   useEffect(() => {
     setIsLoading(true)
     const timer = setTimeout(() => {
@@ -53,31 +51,40 @@ export const coolviewDrawer = ({
 
     return () => clearTimeout(timer);
   }, []);
+  useEffect(() => {
+    const borderElement: any = document.querySelector('.mantine-w29q45');
+
+    if (borderElement) {
+      borderElement.style.borderBottom = isLoading == true ? '' : '2px solid #CED4DA';
+    }
+
+  }, [isLoading]);
 
   return telemetriaStatus === true ? (
     <Drawer
       opened={opened}
       onClose={onClose}
       title={
-        isLoading == true ? '' :   
         <div
           style={{
             display: "flex",
             width: "100%",
             alignItems: "center",
             gap: "4px",
+            visibility: isLoading == true ? 'hidden' : 'visible'
           }}
         >
-          <img
-            src={LogoCoolview}
-            alt={"LogoCoolview"}
-            // className="CoolviewLogo"
-            style={{height:'50px'}}
-          />    
           <div>
-            <div style={{color:'var(--other-black, #000)',fontSize:'1.375rem'}}>Coolview</div>
-            <div style={{color:'var(--Neutral-Outline, #88888B)',fontSize:'0.625rem'}}>Visualización avanzada de telemetría</div>
-          </div>      
+            <div style={{ color: 'var(--other-black, #000)', fontSize: '1.375rem', display: 'flex', alignItems: 'center' }}>
+              <img
+                src={LogoCoolview}
+                alt={"LogoCoolview"}
+                style={{ height: '2rem', width: '2rem' }}
+              />
+              Coolview
+            </div>
+            <div style={{ color: 'var(--Neutral-Outline, #88888B)', fontSize: '0.625rem', marginLeft: '2rem' }}>Visualización avanzada de telemetría</div>
+          </div>
         </div>
       }
       position="bottom"
@@ -85,42 +92,42 @@ export const coolviewDrawer = ({
       overlayOpacity={0.1}
       overlayBlur={0}
       className="drawerCoolview"
+      withCloseButton={false}
     >
       <section className="coolview_printipal">
         {
-          isLoading == true ? 
-          <div style={{width:'100%',height:'95%',display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <Player src={CoolviewAnimationOpen} autoplay style={{ height: '300px', width: '300px' }} loop/>
-          </div>
-          : <iframe src={url2} width="100%" height="95%" frameBorder="0" className={`coolview-drawer ${isVisible ? 'visible' : ''}`}/>
+          isLoading == true ?
+            <div style={{ width: '100%', height: '95%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Player src={CoolviewAnimationOpen} autoplay style={{ height: '300px', width: '300px' }} loop />
+            </div>
+            : <iframe src={url2} width="100%" height="85%" frameBorder="0"/>
         }
-        <div style={{width:'100%',height:'15%',background:'',color:'var(--Neutral-Outline, #88888B)',display:'flex',alignItems:'center',justifyContent:'center'}}>powered by solkos</div>
+        <div style={{ width: '100%', height: 'max-content', background: '', color: 'var(--Neutral-Outline, #88888B)', display: 'flex', alignItems: 'center', justifyContent: 'center',zIndex:'1' ,marginTop:'-1rem' }}>powered by solkos</div>
       </section>
     </Drawer>
   ) : (
     <Drawer
       opened={opened}
       onClose={onClose}
-      title={    
-        isLoading == true ? '' :    
+      title={
         <div
           style={{
-            display:  "flex",
+            display: "flex",
             width: "100%",
             alignItems: "center",
-            gap: "4px",
+            gap: "0.5rem",
+            visibility: isLoading == true ? 'hidden' : 'visible'
           }}
-        >
-          <img
-            src={LogoCoolview}
-            alt={"LogoCoolview"}
-            // className="CoolviewLogo"
-            style={{height:'50px'}}
-          />    
-          <div>
-            <div style={{color:'var(--other-black, #000)',fontSize:'1.375rem'}}>Coolview</div>
-            <div style={{color:'var(--Neutral-Outline, #88888B)',fontSize:'0.625rem'}}>Visualización avanzada de telemetría</div>
-          </div>      
+        >          
+            <div style={{ color: 'var(--other-black, #000)', fontSize: '1.375rem',height:'fit-content' }}>
+              <img
+                src={LogoCoolview}
+                alt={"LogoCoolview"}
+                style={{ height: '2rem', width: '2rem' }}
+              />
+              Coolview
+            </div>
+            <div style={{ color: 'var(--Neutral-Outline, #88888B)', fontSize: '0.625rem', marginLeft: '2rem',height:'fit-content' }}>Visualización avanzada de telemetría</div>          
         </div>
       }
       position="bottom"
@@ -130,30 +137,31 @@ export const coolviewDrawer = ({
       className="drawerCoolview"
     >
       {
-        isLoading == true 
-        ? <section style={{width:'100%',height:'100%',display:'flex',justifyContent:'center',alignItems:'center'}}>
-          <Player src={CoolviewAnimationOpen} autoplay style={{ height: '300px', width: '300px' }} loop></Player>
-        </section>  
-        :
-        <section
-          style={{
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Alert
-            variant="light"
-            color={"yellow"}
-            title={"Sin telemetria"}
-            icon={<IconInfoCircle />}
+        isLoading == true
+          ?
+          <section style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Player src={CoolviewAnimationOpen} autoplay style={{ height: '300px', width: '300px' }} loop></Player>
+          </section>
+          :
+          <section
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            Lo sentimos,se realizo la consulta exitosamente, pero este enfriador
-            no posee telemetria para mostrar en Coolview.
-          </Alert>
-        </section>
+            <Alert
+              variant="light"
+              color={"yellow"}
+              title={"Sin telemetria"}
+              icon={<IconInfoCircle />}
+            >
+              Lo sentimos,se realizo la consulta exitosamente, pero este enfriador
+              no posee telemetria para mostrar en Coolview.
+            </Alert>
+          </section>
       }
     </Drawer>
   );
