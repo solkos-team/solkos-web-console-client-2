a302162b74c567456339dc63c385ea08513e1e73
ivanISC95 <79552848+ivanISC95@users.noreply.github.com>
2024-09-25 10:45:57 -0600

    Update Vault Proces 2 UI
    Update UI into Vault Proces 2, add skeleton and validations

--- src/pages/shell/vault/process2/StepFinal/StepFinal.tsx
+++ src/pages/shell/vault/process2/StepFinal/StepFinal.tsx
@@ -15,6 +15,7 @@ export const StepFinal = ({
 }) => {
     const [isLoading, setIsLoading] = useState(true);
     const [coolerData, setCoolersData] = useState([]);
+    const [error,setError] = useState<string | null>()
     const body = { coolers: vaultProces2TransformData(coolersToChange) };
     const fetchData = async () => {
         try {
@@ -24,6 +25,7 @@ export const StepFinal = ({
             setCoolersData(data);
         } catch (error) {
             console.error("Error:", error);
+            setError(error)
         }
     };
     useEffect(() => {
@@ -47,11 +49,11 @@ export const StepFinal = ({
                 />
                 <div className="vault_h2_description">Proceder con los cambios.</div>
             </section>
-            <section style={{ width: "100%", height: "30%", display: "flex" }}>
+            <section style={{ width: "100%", height: "30%", display: "flex" }}>                
                 {
-                    isLoading == true
-                        ? ''
-                        : <InsightsVault />
+                    isLoading == true 
+                    ? <Skeleton style={{width:'98%' ,height:'100%'}} />
+                    : <InsightsVault activateSkeleton={'True'} />
                 }
             </section>
             <section
@@ -65,30 +67,28 @@ export const StepFinal = ({
             >
                 <section className="vault_container_final">
                     {
-                        isLoading == true
-                            ? <Skeleton style={{ width: '10vh', height: '10vh' }} />
-                            :
-                            <>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="40px"
-                                    height="40px"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                >
-                                    <rect width="24" height="24" rx="4" fill="#B2F2BB" />
-                                    <path
-                                        d="M9 12L11 14L15 10M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                                        stroke="#40C057"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                    />
-                                </svg>
-                                <div className="vault_container_final_h1">
-                                    Proceso Completo {coolersToChange.length}
-                                </div>
-                            </>
+                        isLoading == true 
+                        ? <Skeleton style={{width:'40px',height:'40px'}} />
+                        :                        
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="40px"
+                            height="40px"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                        >
+                            <rect width="24" height="24" rx="4" fill="#B2F2BB" />
+                            <path
+                                d="M9 12L11 14L15 10M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                                stroke="#40C057"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
                     }
+                    <div className="vault_container_final_h1">
+                        {isLoading == true ? <Skeleton style={{width:'5rem' ,height:'0.5rem'}}/> : 'Proceso Completo'}                        
+                    </div>
                 </section>
             </section>
             <section
